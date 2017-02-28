d02c18ccd155528cd94fe9e019ef199beb00de67
Trevor Madge <tmadge@datavalet.com>
2017-02-27 21:49:44 -0500

    enabled french, updated address

--- js/custom.js
+++ js/custom.js
@@ -8,49 +8,47 @@
 		$(".navbar-collapse").css({ maxHeight: $(window).height() - $(".navbar-header").height() + "px" });
 });
 //sticky header on scroll
-$(document).ready(function () {
-		$(window).load(function () {
-				$(".sticky").sticky({topSpacing: 0});
-		});
-});
+$(function () {
+  $(window).load(function () {
+    $(".sticky").sticky({topSpacing: 0});
+  });
 
-/* ==============================================
- WOW plugin triggers animate.css on scroll
- =============================================== */
-$(document).ready(function () {
-		var wow = new WOW(
-						{
-								boxClass: 'wow', // animated element css class (default is wow)
-								animateClass: 'animated', // animation css class (default is animated)
-								offset: 100, // distance to the element when triggering the animation (default is 0)
-								mobile: false        // trigger animations on mobile devices (true is default)
-						}
-		);
-		wow.init();
-});
+  /* ==============================================
+  WOW plugin triggers animate.css on scroll
+  =============================================== */
+  var wow = new WOW(
+    {
+      boxClass: 'wow', // animated element css class (default is wow)
+      animateClass: 'animated', // animation css class (default is animated)
+      offset: 100, // distance to the element when triggering the animation (default is 0)
+      mobile: false        // trigger animations on mobile devices (true is default)
+    }
+  );
+  wow.init();
 
+  //parallax
+  $(window).stellar({
+    horizontalScrolling: false,
+    responsive: true/*,
+      scrollProperty: 'scroll',
+      parallaxElements: false,
+      horizontalScrolling: false,
+      horizontalOffset: 0,
+      verticalOffset: 0*/
+  });
 
-//parallax
-$(document).ready(function () {
-		$(window).stellar({
-				horizontalScrolling: false,
-				responsive: true/*,
-				 scrollProperty: 'scroll',
-				 parallaxElements: false,
-				 horizontalScrolling: false,
-				 horizontalOffset: 0,
-				 verticalOffset: 0*/
-		});
-});
+  //transparent header
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $('.transparent-header').css("background", "#252525");
+    } else {
+      $('.transparent-header').css("background", "transparent");
+    }
+  });
 
-
-//transparent header
-$(document).ready(function () {
-		$(window).scroll(function () {
-				if ($(this).scrollTop() > 100) {
-						$('.transparent-header').css("background", "#252525");
-				} else {
-						$('.transparent-header').css("background", "transparent");
-				}
-		});
+  //language changer
+  var pathname = window.location.pathname;
+  var path = $(".fr .lang").length ? pathname.replace(/(\/fr\/|\/fr$)/, '/') : "/fr" + pathname;
+  var url = window.location.protocol + "//" + window.location.host + path;
+  $("a.lang").attr("href", url);
 });
